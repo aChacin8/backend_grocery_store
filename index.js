4b72a0be134a33bda65740b1d241b2938dfd0601
mrMcAko <alechacin08@gmail.com>
2025-04-13 21:51:58 -0600

    Add routes that use Users

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express')
-const productRoutes = require ('./routes/productRoutes')
+const productRoutes = require('./routes/productRoutes')
+const userRoutes = require('./routes/usersRoutes')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(express.json());
+app.use(express.json())
 app.use('/api', productRoutes)
+app.use('/api', userRoutes)
 
-app.listen (port,  () => {
-    console.log(`Server is running on port ${port}`)
-    console.log(`http://localhost:${port}`)
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+  console.log(`http://localhost:${port}`)
 })
