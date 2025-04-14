39ebd015f10d4a9ba1d8c000daea2a6b5137962b
mrMcAko <alechacin08@gmail.com>
2025-04-13 21:54:49 -0600

    Add controllers to function that filters no sales in GDL

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,52 +1,65 @@
-const ModelProduct = require ('../models/ModelProduct')
+const ModelProduct = require('../models/Product')
 
-const createProduct = (req, res)=> {
-    ModelProduct
-        .create(req.body)
-        .then(product => {
-            res.status(201).json(product)
-        })
-        .catch(error => {
-            res.status(400).json({ message: 'Error al crear el producto', error })
-        })
+const createProduct = (req, res) => {
+  ModelProduct
+    .createProduct(req.body)
+    .then(product => {
+      res.status(201).json(product)
+    })
+    .catch(error => {
+      res.status(400).json({ message: 'Error al crear el producto', error })
+    })
 }
 
 const findAllProducts = (req, res) => {
-    ModelProduct
-        .findAll()
-        .then (products => {
-            res.status(200).json(products)
-        })
-        .catch(error => {
-            res.status(400).json({ message: 'Error al encontrar los productos', error })
-        })
+  ModelProduct
+    .findAll()
+    .then(products => {
+      res.status(200).json(products)
+    })
+    .catch(error => {
+      res.status(400).json({ message: 'Error al encontrar los productos', error })
+    })
 }
 
 const findById = (req, res) => {
-    ModelProduct
-        .findById(req.params.idProduct)
-        .then(product => {
-            res.status(200).json(product)
-        })
-        .catch(error => {
-            res.status(400).json({ message: 'Error al encontrar el producto por ID', error })
-        })  
+  ModelProduct
+    .findById(req.params.idProduct)
+    .then(product => {
+      res.status(200).json(product)
+    })
+    .catch(error => {
+      res.status(400).json({ message: 'Error al encontrar el producto por ID', error })
+    })
 }
 
-const findLess15 = (req, res) => {
-    ModelProduct
-        .less15()
-        .then(products =>{
-            res.status(200).json(products)
-        })
-        .catch(error => {
-            res.status(400).json({ message: 'Error al encontrar los productos con stock menor a 15', error })
-        })
+const findLowPriceProducts = (req, res) => {
+  ModelProduct
+    .findLowPrice()
+    .then(products => {
+      res.status(200).json(products)
+    })
+    .catch(error => {
+      res.status(400).json({ message: 'Error al encontrar los productos con stock menor a 15', error })
+    })
+}
+
+const noSalesProductsGdl = (req, res) => {
+  ModelProduct
+    .noSalesGdl()
+    .then(products => {
+      res.status(200).json(products)
+    })
+    .catch(error => {
+      res.status(400).json({ message: 'Error al encontrar los productos sin ventas en Guadalajara', error })
+    }
+    )
 }
 
 module.exports = {
-    createProduct,
-    findAllProducts,
-    findById,
-    findLess15
+  createProduct,
+  findAllProducts,
+  findById,
+  findLowPriceProducts,
+  noSalesProductsGdl
 }
