908d99fdb5ac8cb19d49dbc5d0c62e1b319f6547
mrMcAko <alechacin08@gmail.com>
2025-04-13 18:41:20 -0600

    Modify references to model

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,4 +1,4 @@
-const ModelProduct = require ('../models/Product')
+const ModelProduct = require ('../models/ModelProduct')
 
 const createProduct = (req, res)=> {
     ModelProduct
@@ -13,7 +13,7 @@ const createProduct = (req, res)=> {
 
 const findAllProducts = (req, res) => {
     ModelProduct
-        .findAllProducts()
+        .findAll()
         .then (products => {
             res.status(200).json(products)
         })
@@ -22,7 +22,31 @@ const findAllProducts = (req, res) => {
         })
 }
 
+const findById = (req, res) => {
+    ModelProduct
+        .findById(req.params.idProduct)
+        .then(product => {
+            res.status(200).json(product)
+        })
+        .catch(error => {
+            res.status(400).json({ message: 'Error al encontrar el producto por ID', error })
+        })  
+}
+
+const findLess15 = (req, res) => {
+    ModelProduct
+        .less15()
+        .then(products =>{
+            res.status(200).json(products)
+        })
+        .catch(error => {
+            res.status(400).json({ message: 'Error al encontrar los productos con stock menor a 15', error })
+        })
+}
+
 module.exports = {
     createProduct,
-    findAllProducts
+    findAllProducts,
+    findById,
+    findLess15
 }
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,8 +1,11 @@
-const exoress = require('express')
-const router = XPathExpression.Router()
+const express = require('express')
+const router = express.Router()
 
 const productController = require('../controllers/productController')
 
 router.post('/products', productController.createProduct)
+router.get('/products', productController.findAllProducts)
+router.get('/products/:idProduct', productController.findById)
+router.get('/product/price15', productController.findLess15)
 
 module.exports = router
